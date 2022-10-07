5757d3f24d78807c50ce692df486e039b753534f
Pervaiz Farah <100537066+pervaizf@users.noreply.github.com>
2022-10-06 23:51:02 -0400

    Create Scramblies.js

---
+++ 6kyu/Scramblies.js
@@ -0,0 +1,4 @@
+function scramble(str1, str2) {
+    let occurences = str1.split("").reduce((arr, cur) => { arr[cur] ? arr[cur]++ : arr[cur] = 1; return arr; }, {});
+    return str2.split("").every((character) => --occurences[character] >= 0);
+  }
